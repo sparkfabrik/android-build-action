67229c9587114cc53002c9b5972fd6432160a585
Emiliano Morghen <emiliano.morghen@sparkfabrik.com>
2023-01-26 17:42:33 +0100

    #refs 16: add rvm to $USER group

--- build.sh
+++ build.sh
@@ -12,6 +12,9 @@ sudo apt-add-repository -y ppa:rael-gc/rvm
 sudo apt-get update
 sudo apt-get install rvm
 
+sudo usermod -a -G rvm $USER
+echo 'source "/etc/profile.d/rvm.sh"' >> ~/.bashrc
+
 . ~/.rvm/scripts/rvm
 
 rvm -v
