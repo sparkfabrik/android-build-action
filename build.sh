df52f2c5d157ca6885b9414c80dc4cc0511da1a5
Emiliano Morghen <emiliano.morghen@sparkfabrik.com>
2023-01-26 20:16:53 +0100

    refs #16: Fix on gem install bundler

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ if [ -n "${RUBY_VERSION}" ]; then
     source /etc/profile.d/rvm.sh
 
     rvm -v && ruby -v
-    rvm install ${RUBY_VERSION}
+    # rvm install ${RUBY_VERSION}
     # rvm --default use ${RUBY_VERSION}
     echo "Running ruby with version: ${RUBY_VERSION}"
     ruby -v
@@ -29,7 +29,7 @@ fi
 # otherwise use latest
 if [ -n "${BUNDLER_VERSION}" ]; then
     echo "Running bundler with version: ${BUNDLER_VERSION}"
-    sudo gem install bundler -NV -v ${BUNDLER_VERSION}
+    sudo gem install bundler:${BUNDLER_VERSION} -NV
 else
     sudo gem install bundler -NV
 fi
