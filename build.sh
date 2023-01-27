b3c46aaa3e9ef849bfd97c3a8f1eaa9695457493
Emiliano Morghen <emiliano.morghen@sparkfabrik.com>
2023-01-27 14:45:59 +0100

    refs #16: Implemented logic into build.sh

--- build.sh
+++ build.sh
@@ -1,73 +1,57 @@
-echo "HELLO WORLD" + ${OUTPUT_PATH}
+#!/bin/bash -e
 
-# #!/bin/bash -e
-# OUTPUT_PATH="${PWD}/${OUTPUT_PATH}"
+if [ "${BUILD_TYPE}" != "assemble" || "${BUILD_TYPE}" != "bundle" ]; then
+    echo "Invalid build-type: " ${BUILD_TYPE}
+    exit 1
+fi
 
-# # If the variable KEYSTORE_CONTENT is set, then the keystore is created from the content of the variable.
-# if [ -n "${KEYSTORE_CONTENT}" ]; then
-#     echo "Creating keystore from content"
-#     echo $KEYSTORE_CONTENT | base64 --decode > "keystore.jks"
-# fi
+if [ "${UPLOAD_TO_PLAY_STORE}" == true ]; then
+    if [ -n "${PACKAGE_NAME}" ]; then
+        echo "package-name is required when uploading to play store"
+        exit 2
+    elif [ -n "${KEYSTORE_CONTENT}" ]; then
+        echo "Missing keystore-content"
+        exit 3
+    elif [ -n "${KEYSTORE_PASSWORD}" ]; then
+        echo "Missing keystore-password"
+        exit 4
+    elif [ -n "${KEYSTORE_ALIAS}" ]; then
+        echo "Missing keystore-alias"
+        exit 5
+    elif [ -n "${JSON_KEY_DATA}" ]; then
+        echo "Missing json-key-data"
+        exit 6
+    fi
+fi
 
-# #TODO: Make sure BUNDLER_VERSION and RUBY_VERSION are compatible
+OUTPUT_PATH="${PWD}/${OUTPUT_PATH}"
 
-# # If RUBY_VERSION is set, then latest RVM is installed and the specified version of Ruby will be used
-# if [ -n "${RUBY_VERSION}" ]; then
-#     # sudo apt-get update 
-#     # sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev libffi-dev
-    
-#     # git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-#     # echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-#     # echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-#     # exec $SHELL
-    
-#     # git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-#     # echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-#     # exec $SHELL
+# If the variable KEYSTORE_CONTENT is set, then the keystore is created from the content of the variable.
+if [ -n "${KEYSTORE_CONTENT}" ]; then
+    echo "Creating keystore from content"
+    echo $KEYSTORE_CONTENT | base64 --decode > "keystore.jks"
+fi
 
-#     # rbenv -v
-#     # rbenv install ${RUBY_VERSION}
-#     # rbenv global ${RUBY_VERSION}
-#     # ruby -v
+# If the variable BUNDLER_VERSION is set, then install bundler with selected version
+# otherwise use latest
+if [ -n "${BUNDLER_VERSION}" ]; then
+    echo "Running bundler with version: ${BUNDLER_VERSION}"
+    gem install bundler:${BUNDLER_VERSION} -NV
+else
+    gem install bundler -NV
+fi
 
-#     # sudo apt-get install software-properties-common
-#     # sudo apt-add-repository -y ppa:rael-gc/rvm
-#     # sudo apt-get update
-#     # sudo apt-get install rvm
+script_path=$(cd $(dirname ${0}); pwd)
+cp -r ${script_path}/fastlane ./
+cp -r ${script_path}/Gemfile ./
 
-#     # source /etc/profile.d/rvm.sh
+bundle install
 
-#     # sudo rvm get stable
-#     # sudo rvm -v && ruby -v
-#     # sudo rvm install ${RUBY_VERSION}
-#     # sudo rvm use ${RUBY_VERSION}
-#     # ruby -v
-#     # echo "Running ruby with version: ${RUBY_VERSION}"
-#     # ruby -v
-# fi
-
-# # If the variable BUNDLER_VERSION is set, then install bundler with selected version
-# # otherwise use latest
-# if [ -n "${BUNDLER_VERSION}" ]; then
-#     echo "Running bundler with version: ${BUNDLER_VERSION}"
-#     gem install bundler:2.3.26 -NV
-#     rbenv rehash
-# else
-#     gem install bundler -NV
-# fi
-
-
-# script_path=$(cd $(dirname ${0}); pwd)
-# cp -r ${script_path}/fastlane ./
-# cp -r ${script_path}/Gemfile ./
-
-# bundle install
-
-# # If the variable FASTLANE_ENV is set then run fastlane with the --env equal to the variable.
-# if [ -n "${FASTLANE_ENV}" ]; then
-#     echo "Running fastlane with environment: ${FASTLANE_ENV}"
-#     fastlane --env ${FASTLANE_ENV} android $BUILD_TYPE
-# else
-#     echo "Running fastlane"
-#     fastlane android $BUILD_TYPE
-# fi
+# If the variable FASTLANE_ENV is set then run fastlane with the --env equal to the variable.
+if [ -n "${FASTLANE_ENV}" ]; then
+    echo "Running fastlane with environment: ${FASTLANE_ENV}"
+    fastlane --env ${FASTLANE_ENV} android $BUILD_TYPE
+else
+    echo "Running fastlane"
+    fastlane android $BUILD_TYPE
+fi
