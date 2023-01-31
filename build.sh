efc81f313fb6ed973dc9fb02c1dd350c520fd911
Emiliano Morghen <emiliano.morghen@sparkfabrik.com>
2023-01-31 16:13:18 +0100

    refs #16: fixed condition syntax in build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ if [ "${BUILD_TYPE}" != "assemble" -a "${BUILD_TYPE}" != "bundle" ]; then
     exit 1
 fi
 
-if [ "${UPLOAD_TO_PLAY_STORE}" == true ]; then
+if [ -n "${UPLOAD_TO_PLAY_STORE}"]; then
     if [ -n "${PACKAGE_NAME}" ]; then
         echo "package-name is required when uploading to play store"
         exit 2
