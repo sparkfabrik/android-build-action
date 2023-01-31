eb8cced2ffc78a9b1c182e641c98f3c229c4af24
Emiliano Morghen <emiliano.morghen@gmail.com>
2023-01-31 18:51:34 +0100

    refs #16: default bundler-version is 2.3, check gemfile.lock for correct bundler version (#22)
    Co-authored-by: Emiliano Morghen <emiliano.morghen@sparkfabrik.com>

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -e
-
-echo "BUILD TYPE: ${BUILD_TYPE}"
-if [ "${BUILD_TYPE}" != "assemble" || "${BUILD_TYPE}" != "bundle" ]; then
+if [ "${BUILD_TYPE}" != "assemble" -a "${BUILD_TYPE}" != "bundle" ]; then
     echo "Invalid build-type: " ${BUILD_TYPE}
     exit 1
 fi
 
-if [ "${UPLOAD_TO_PLAY_STORE}" == true ]; then
+if [ "${UPLOAD_TO_PLAY_STORE}" = "true" ]; then
     if [ -n "${PACKAGE_NAME}" ]; then
         echo "package-name is required when uploading to play store"
         exit 2
@@ -34,11 +32,16 @@ if [ -n "${KEYSTORE_CONTENT}" ]; then
     echo $KEYSTORE_CONTENT | base64 --decode > "keystore.jks"
 fi
 
+cp -r ${ACTION_PATH}/fastlane ./
+cp -r ${ACTION_PATH}/Gemfile ./
+
+bundle install
+
 # If the variable FASTLANE_ENV is set then run fastlane with the --env equal to the variable.
 if [ -n "${FASTLANE_ENV}" ]; then
     echo "Running fastlane with environment: ${FASTLANE_ENV}"
-    fastlane --env ${FASTLANE_ENV} android $BUILD_TYPE
+    bundle exec fastlane --env ${FASTLANE_ENV} android $BUILD_TYPE
 else
     echo "Running fastlane"
-    fastlane android $BUILD_TYPE
+    bundle exec fastlane android $BUILD_TYPE
 fi
